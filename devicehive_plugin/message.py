4f3420c672e97f3793f9c118a368419bec1cdc10
platon <platon.dimka@gmail.com>
2018-01-16 11:49:06 +0300

    Add constants and props

--- devicehive_plugin/message.py
+++ devicehive_plugin/message.py
@@ -1,13 +1,41 @@
 class Message(object):
 
+    TYPE_KEY = 't'
+
     ACTION_KEY = 'a'
+
+    PAYLOAD_KEY = 'p'
+
+    SUCCESS_KEY = 's'
 
     AUTHENTICATE_ACTION = 'authenticate'
 
-    def __init__(self, message):
-        self._message = message
-        self._action = message[self.ACTION_KEY]
+    SUBSCRIBE_ACTION = 'subscribe'
+
+    UNSUBSCRIBE_ACTION = 'unsubscribe'
+
+    SUCCESS = 1
+
+    def __init__(self, message=None):
+        if not message:
+            return
+        self.type = message[self.TYPE_KEY]
+        self.action = message[self.ACTION_KEY]
+        self.payload = message[self.PAYLOAD_KEY]
+        self.success = message.get(self.SUCCESS_KEY)
 
     @property
     def is_authenticate(self):
-        return self._action == self.AUTHENTICATE_ACTION
+        return self.action == self.AUTHENTICATE_ACTION
+
+    @property
+    def is_subscribe(self):
+        return self.action == self.SUBSCRIBE_ACTION
+
+    @property
+    def is_unsubscribe(self):
+        return self.action == self.UNSUBSCRIBE_ACTION
+
+    @property
+    def is_success(self):
+        return self.success == self.SUCCESS
