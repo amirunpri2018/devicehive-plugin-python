6783a079519de7f676c806305f13e52f19bbcc1f
Igor Panteleev <ipanteleyev@dataart.com>
2018-01-16 16:58:31 +0200

    Add logging to example.py

--- example.py
+++ example.py
@@ -1,5 +1,15 @@
+import logging
+
 from devicehive_plugin.handler import Handler
 from devicehive_plugin.plugin import Plugin
+
+
+handler = logging.StreamHandler()
+handler.setLevel('DEBUG')
+logger = logging.getLogger('devicehive_plugin')
+logger.addHandler(handler)
+logger.setLevel('DEBUG')
+
 
 access_token = 'eyJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjp7ImEiOlswXSwiZSI6MjA4MzY3NDI1ODc4MiwidCI6MSwidHBjIjoicGx1Z2luX3RvcGljXzgxNTkwNGNmLWU1YzUtNGViYy04NjNkLTUyYjNmOTNmMGNmMCJ9fQ.52qdDOSDWfr-PiI68R-MZjCV1MIOOAaI0AXEThlRyoQ'
 url = 'ws://127.0.0.1:3000'
